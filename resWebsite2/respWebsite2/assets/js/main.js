558f731389761381ff1a2d2b68730ce28ab7c692
jane ramos <hjbibaud@gmail.com>
2019-06-05 01:09:26 -0400

    intial commit

---
+++ resWebsite2/respWebsite2/assets/js/main.js
@@ -0,0 +1,21 @@
+var btn = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
